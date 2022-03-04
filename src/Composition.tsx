f8bd7217047a3e3bb57a2536953adb8a689051d4
Cal Colson <14209384+CalColson@users.noreply.github.com>
2022-03-04 15:15:13 -0600

    finish tutorial

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,26 @@
+import { useCurrentFrame, useVideoConfig } from "remotion";
+import { France } from "./France";
+import { Rain } from "./Rain";
+
 export const MyComposition = () => {
-	return null;
+	const { fps, durationInFrames, width, height } = useVideoConfig()
+
+	const frame = useCurrentFrame()
+	const opacity = frame / durationInFrames
+
+	return (
+		<div style={{
+			flex: 1,
+			textAlign: 'center',
+			fontSize: '7em',
+			color: 'red',
+			backgroundColor: 'black',
+
+			// opacity: opacity
+		}}>
+			hello calbot! 👋
+			<France/>
+			<Rain />
+		</div>
+	)
 };
